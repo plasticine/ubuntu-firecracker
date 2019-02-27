4fc352085c0681fe75d54ef1dfcf1f56f86a2869
Justin Morris <desk@pixelbloom.com>
2019-02-28 08:37:03 +1100

    WIP

--- script/provision.sh
+++ script/provision.sh
@@ -4,7 +4,9 @@ set -ex
 dpkg -i /mnt/root/linux*.deb
 
 echo 'ubuntu-bionic' > /etc/hostname
+
 passwd -d root
+
 mkdir /etc/systemd/system/serial-getty@ttyS0.service.d/
 cat <<EOF > /etc/systemd/system/serial-getty@ttyS0.service.d/autologin.conf
 [Service]
@@ -21,3 +23,13 @@ network:
       dhcp4: true
 EOF
 netplan generate
+
+echo "UseDNS no" >> /etc/ssh/sshd_config
+
+sh -c 'echo deb https://apt.buildkite.com/buildkite-agent stable main > /etc/apt/sources.list.d/buildkite-agent.list'
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 32A37959C2FA5C3C99EFBC32A79206696452D198
+
+apt-get update
+apt-get install -y --no-install-recommends buildkite-agent
+systemctl enable buildkite-agent
+systemctl start buildkite-agent
